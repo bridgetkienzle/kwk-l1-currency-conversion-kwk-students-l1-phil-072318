af50fbfe69f104d629fe53d7ebf52a61aeb883e7
Bridget Kienzle <bridgetkien@gmail.com>
2018-07-26 13:45:26 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -1,11 +1,11 @@
 # Write your cod here
 
-def usd_eur
-  puts "How much money are you converting?"
-  cash = gets.chomp
-  money = cash.to_f * 1.20
-  puts "it's worth #{money} euro"
-end
+# def usd_eur
+#   puts "How much money are you converting?"
+#   cash = gets.chomp
+#   money = cash.to_f * 1.20
+#   puts "it's worth #{money} euro"
+# end
 
 usd_eur()
 
