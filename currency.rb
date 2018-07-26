9d55a3077f25a13bef9162b7039403feeffd51e6
Bridget Kienzle <bridgetkien@gmail.com>
2018-07-26 13:48:16 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -12,7 +12,7 @@
 def eur_usd
   puts "How much money are you converting?"
   gets.chomp = cash.to_i
-  cash.to_f % 1.20 = money.to_f
+  cash.to_f % 1.20 =money.to_f
     puts "it's worth #{money} usd"
 end
 
